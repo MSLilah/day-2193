9b1a788f53b37570e383613885c14bc666309982
Brian Cain <brian.cain@puppetlabs.com>
2016-01-30 08:34:07 -0800

    Add basic board and game manager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+  private Transform boardTransform;
+  private int columns;
+  private int rows;
+
+  public GameObject wall;
+  public GameObject floor;
+
+  void DrawBoard() {
+    GameObject instObj;
+    for (int i = 0; i < columns; i++) {
+      for (int j = 0; j < rows; j++) {
+        if ((i > 0 && j > 0) || (i < columns && j < rows)) {
+          instObj = wall;
+        } else {
+          instObj = floor;
+        }
+
+        GameObject instance = Instantiate(instObj,
+            new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
+        instance.transform.SetParent(boardTransform);
+      }
+
+    }
+  }
+
+  public void GenerateBoard(GameObject w, GameObject f) {
+    wall = w;
+    floor = f;
+
+    DrawBoard();
+  }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+  private BoardManager boardManager;
+
+  public GameObject wall;
+  public GameObject floor;
+
+  void StartGame() {
+    Debug.Log("Hello there");
+    boardManager.GenerateBoard(wall, floor);
+  }
+
+  void Awake() {
+    boardManager = GetComponent<BoardManager>();
+    StartGame();
+  }
+}
