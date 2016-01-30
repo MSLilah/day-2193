05a91d984f781052e37d614446ef03e3fab44ddc
Preben Ingvaldsen <preben@puppetlabs.com>
2016-01-30 08:23:20 -0800

    Add Protagonist prefab
    Add a prefab for the protagonist with a placeholder sprite.
    Add a script that allows the protagonist to be moved.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+  public float playerSpeed;
+
+  private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+	  rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  Move();
+	}
+
+  void Move() {
+    float xVel = Input.GetAxisRaw("Horizontal") * playerSpeed;
+    float yVel = Input.GetAxisRaw("Vertical") * playerSpeed;
+
+    rb.velocity = new Vector2(xVel, yVel);
+  }
+}
