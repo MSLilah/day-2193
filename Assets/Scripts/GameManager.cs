50b8a8c73e2d25d1d25253d86a5301ae29cdd1d4
Preben Ingvaldsen <preben@puppetlabs.com>
2016-01-30 08:58:35 -0800

    Add increasing course offset
    Add a course offset that increases over time. Update on-screen
    text with the display of the offset value.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
@@ -12,9 +13,21 @@ public class GameManager : MonoBehaviour {
   public GameObject floor;
   public GameObject robot;
 
+  //TODO: Remove this element as it is here for debugging purposes. We will
+  // have a nicer display later
+  public Text courseOffsetDisplay;
+
+  private float courseOffset;
+  private float courseRateOfIncrease;
+
+  private const int MAX_COURSE_OFFSET = 100;
+
   void StartGame() {
     Debug.Log("Hello there");
     boardManager.GenerateBoard(wall, floor);
+
+    courseOffset = 15f;
+    courseRateOfIncrease = 0.5f;
   }
 
   void GameOver() {
@@ -27,5 +40,10 @@ public class GameManager : MonoBehaviour {
   void Awake() {
     boardManager = GetComponent<BoardManager>();
     StartGame();
+  }
+
+  void Update() {
+    courseOffset += courseRateOfIncrease * Time.deltaTime;
+    courseOffsetDisplay.text = "Course Offset: " + Mathf.FloorToInt(courseOffset);
   }
 }
