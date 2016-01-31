ab27e01558f9d0f37cf05674fea57caf87d8599e
Brian Cain <brian.cain@puppetlabs.com>
2016-01-30 17:11:35 -0800

    Display time UI element

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,17 +5,23 @@ using System.Collections;
 public class UIController : MonoBehaviour {
   public Text[] uiDisplays;
   public Text healthDisplay;
+  public Text timeDisplay;
 
   private GameObject[] restorationStations;
   private PlayerController pc;
+  private GameManager gm;
 
   void Start() {
     restorationStations = GameObject.FindGameObjectsWithTag(Tags.RESTORATION_STATION);
     pc = GameObject.FindGameObjectWithTag(Tags.PLAYER).GetComponent<PlayerController>();
+    gm = GameObject.FindGameObjectWithTag(Tags.GAME_CONTROLLER).GetComponent<GameManager>();
   }
 
   void Update() {
     healthDisplay.text = "Health: " + Mathf.CeilToInt(pc.health);
+
+    timeDisplay.text = "Time: " + gm.timeLeft.ToString("F2");
+
     int i = 0;
     foreach (GameObject obj in restorationStations) {
       RestorationStationController rsc = obj.GetComponent<RestorationStationController>();
