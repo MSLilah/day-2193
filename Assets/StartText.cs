4bde478723215f83043a5bc4e6ed23ea3cd1a3f4
Preben Ingvaldsen <preben@puppetlabs.com>
2016-01-30 22:19:50 -0800

    Add opening fade

---
+++ Assets/StartText.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartText : MonoBehaviour {
+
+  public GameObject pressToStart;
+
+  public void FadeNextText() {
+    pressToStart.GetComponent<StartGame>().StartAnimation();
+  }
+}
