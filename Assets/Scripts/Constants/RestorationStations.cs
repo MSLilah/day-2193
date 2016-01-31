83dae6da9efab8fd00635d0480596ed3bf28e0be
Brian Cain <brian.cain@puppetlabs.com>
2016-01-30 22:17:40 -0800

    Only decrease resources if attacked

--- Assets/Scripts/Constants/RestorationStations.cs
+++ Assets/Scripts/Constants/RestorationStations.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class RestorationStations : MonoBehaviour {
   public const string CONSOLE = "Console";
   public const string OXYGEN_STATION = "OxygenStation";
-  public const string ITEM_STATION = "ItemStation";
+  public const string RESOURCE_STATION = "ResourceStation";
   public const string HEALTH_STATION = "HealthStation";
 }
