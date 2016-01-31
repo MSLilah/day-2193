a959f7cd0df7e1f3840306e9384f8b1bd4c1ea1a
Preben Ingvaldsen <preben@puppetlabs.com>
2016-01-30 16:38:53 -0800

    Refactor handling of restoration stations
    CHange the implementation of restoration stations so that the
    station itself is responsible for changing the value of a
    resource, rather than having it be controlled by the GameManager.
    Implement a new UI controller to display the debug UI values.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,30 +10,11 @@ public class GameManager : MonoBehaviour {
   public GameObject robot;
   public GameObject projectile;
 
-  //TODO: Remove this element as it is here for debugging purposes. We will
-  // have a nicer display later
-  public Text courseOffsetDisplay;
-  public Text oxygenDisplay;
-  public Text itemDisplay;
-
   private BoardManager boardManager;
 
-  private static float courseOffset;
-  private float courseRateOfIncrease;
-  private float courseRateOfRestoration;
   public GameObject console;
-
-  private float currentOxygen;
-  private float oxygenRateOfDecrease;
-  private float oxygenRateOfRestoration;
   public GameObject oxygenStation;
-
-  private float currentItems;
-  private float itemsRateOfDecrease;
-  private float itemsRateOfRestoration;
   public GameObject resourceStation;
-
-  private float healthRateOfRestoration;
   public GameObject healthStation;
 
   private float timeLeft;
@@ -46,25 +27,11 @@ public class GameManager : MonoBehaviour {
   void StartGame() {
     boardManager.PlaceObj(robot, new Vector3(2.0f, 4.0f, 0f));
 
-    courseOffset = 15f;
-    courseRateOfIncrease = 0.5f;
-    courseRateOfRestoration = -1.0f;
-
     boardManager.PlaceObj(console, new Vector3(8.0f, 3.0f, 0f));
-
-    currentOxygen = 100f;
-    oxygenRateOfDecrease = 1.0f;
-    oxygenRateOfRestoration = 2.0f;
 
     boardManager.PlaceObj(oxygenStation, new Vector3(23f, 17f, 0f));
 
-    currentItems = 20f;
-    itemsRateOfDecrease = 0.2f;
-    itemsRateOfRestoration = 0.3f;
-
     boardManager.PlaceObj(resourceStation, new Vector3(-12f, 13f, 0f));
-
-    healthRateOfRestoration = 5.0f;
 
     timeLeft = 60f;
 
@@ -74,26 +41,15 @@ public class GameManager : MonoBehaviour {
     boardManager.GenerateBoard(wall, floor);
   }
 
-  void GameOver() {
+  public void GameOver() {
     Debug.Log("Game Over");
     SceneManager.LoadScene(Scenes.GAME_OVER);
   }
 
-  bool IsGameOver() {
-    // End conditions
-    //
-    if (!pc.IsAlive()) {
-      return true;
-    } else if (timeLeft < 0f) {
-      return true;
-    } else if (currentOxygen <= 0) {
-      return true;
-    } else if (currentItems <= 0) {
-      return true;
-    } else if (courseOffset >= 100) {
-      return true;
-    } else {
-      return false;
+  void Update() {
+    timeLeft -= Time.deltaTime;
+    if (timeLeft <= 0) {
+      WinGame();
     }
   }
 
@@ -104,63 +60,5 @@ public class GameManager : MonoBehaviour {
   void Awake() {
     boardManager = GetComponent<BoardManager>();
     StartGame();
-  }
-
-  void Update() {
-    if (IsGameOver()) {
-      GameOver();
-    }
-
-    // Decrease resources
-    PeriodicDecreaseResources();
-  }
-
-  void PeriodicDecreaseResources() {
-    courseOffset += courseRateOfIncrease * Time.deltaTime;
-    currentOxygen -= Time.deltaTime * oxygenRateOfDecrease;
-    currentItems -= Time.deltaTime * itemsRateOfDecrease;
-    timeLeft -= Time.deltaTime;
-
-    // TODO: Remove these in favor of a more interesting interface
-    courseOffsetDisplay.text = "Course Offset: " + Mathf.FloorToInt(courseOffset);
-    oxygenDisplay.text = "Oxygen: " + Mathf.CeilToInt(currentOxygen);
-    itemDisplay.text = "Items: " + Mathf.CeilToInt(currentItems);
-  }
-
-  public void RestoreResource(GameObject restoringStation) {
-    switch (restoringStation.name) {
-      case RestorationStations.CONSOLE:
-        courseOffset += courseRateOfRestoration * Time.deltaTime;
-        break;
-      case RestorationStations.OXYGEN_STATION:
-        currentOxygen += oxygenRateOfRestoration * Time.deltaTime;
-        break;
-      case RestorationStations.ITEM_STATION:
-        currentItems += itemsRateOfRestoration * Time.deltaTime;
-        break;
-      case RestorationStations.HEALTH_STATION:
-        pc.RestoreHealth(healthRateOfRestoration * Time.deltaTime);
-        break;
-      default:
-        // Do nothing, as this was errantly triggered
-        break;
-    }
-  }
-
-  public void DecreaseResource(GameObject restoringStation) {
-    switch (restoringStation.name) {
-      case RestorationStations.CONSOLE:
-        courseOffset += 4 * courseRateOfIncrease * Time.deltaTime;
-        break;
-      case RestorationStations.OXYGEN_STATION:
-        currentOxygen -= 4 * oxygenRateOfDecrease * Time.deltaTime;
-        break;
-      case RestorationStations.ITEM_STATION:
-        currentItems -= 4 * itemsRateOfDecrease * Time.deltaTime;
-        break;
-      default:
-        // Do nothing, as this was errantly triggered
-        break;
-    }
   }
 }
---
+++ Assets/Scripts/RestorationStationController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RestorationStationController : MonoBehaviour {
+
+	public string name;
+  public string resourceName;
+
+  public float resourceReductionRate;
+  public float resourceRestorationRate;
+
+  public float resourceTotal;
+
+  private float resourceGameOverValue;
+  private float resourceMaxValue;
+  private GameManager gm;
+
+  void Start() {
+    resourceGameOverValue = 0f;
+    resourceMaxValue = 100f;
+    gm = GameObject.FindGameObjectWithTag(Tags.GAME_CONTROLLER).GetComponent<GameManager>();
+  }
+
+  void Update() {
+    // Decrease the resource value
+    DecreaseResourceTotal(resourceReductionRate * Time.deltaTime);
+  }
+
+  public void DecreaseResourceTotal(float reduction) {
+    resourceTotal -= reduction;
+    if (resourceTotal <= resourceGameOverValue) {
+      gm.GameOver();
+    }
+  }
+
+  public void RestoreResource() {
+    float increase = resourceRestorationRate * Time.deltaTime;
+    resourceTotal = Mathf.Min(resourceTotal + increase, resourceMaxValue);
+  }
+
+  public void DamageResource() {
+    DecreaseResourceTotal(3 * resourceReductionRate * Time.deltaTime);
+  }
+
+  public string GenerateText() {
+    return resourceName + ": " + Mathf.CeilToInt(resourceTotal);
+  }
+}
