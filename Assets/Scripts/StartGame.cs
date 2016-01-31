1916a40ab72e7bdd69d7eecf072e9fb8db3a669a
Preben Ingvaldsen <preben@puppetlabs.com>
2016-01-30 22:19:50 -0800

    Add opening fade

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+  private Animator anim;
+  private bool canPlay = false;
+  
+  
+  void Start() {
+    anim = gameObject.GetComponent<Animator>();
+  }
+
+	void Update() {
+    if (canPlay && Input.GetKey(KeyCode.Space)) {
+      SceneManager.LoadScene(Scenes.MAIN_GAME);
+    }
+  }
+
+  public void StartAnimation() {
+    anim.SetTrigger("Play");
+  }
+
+  public void AllowStart() {
+    print("Allowing start");
+    canPlay = true;
+  }
+}
